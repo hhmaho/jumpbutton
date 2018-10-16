76cd93ac6f6bbb715854125fd3ca78abee267702
hhmaho <42540357+hhmaho@users.noreply.github.com>
2018-10-16 15:46:51 +0200

    button component

---
+++ jumpbutton/src/Component/Button.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class Button extends React.Component {
+    render() {
+        return (
+            <button style={{ "marginTop": "100px", "marginLeft": "100px" }}>
+                click me!
+        </button>
+        )
+    }
+}
+
+export default Button
