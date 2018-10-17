5999eff71ec6c9007d07db913d00b7804d35f757
hhmaho <42540357+hhmaho@users.noreply.github.com>
2018-10-17 18:00:44 +0200

    in progress

--- jumpbutton/src/Component/Button.js
+++ jumpbutton/src/Component/Button.js
@@ -1,11 +1,31 @@
 import React from 'react';
 
+function changePosition(min, max) {
+    return Math.floor(Math.random() * (max - min)) + min;
+}
+
+//console.log(moveButton(-100, 100))
+
 class Button extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { position }
+    }
+    state = {}
     render() {
         return (
-            <button style={{ "marginTop": "100px", "marginLeft": "100px" }}>
+            <button style={{ "marginTop": "100px", "marginLeft": "100px" }}
+                onClick={() => {
+                    let position = 0;
+                    if (this.state.position > 0) {
+                        return
+                    }
+                    this.setState({
+                        randomPosition: this.state.randomPosition + randomPosition
+                    })
+                }}>
                 click me!
-        </button>
+            </button>
         )
     }
 }
