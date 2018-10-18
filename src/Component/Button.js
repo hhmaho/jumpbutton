b85f566f79927d3a3c62aa375ebd826d0392c883
hhmaho <42540357+hhmaho@users.noreply.github.com>
2018-10-18 11:15:01 +0200

    x y random

--- src/Component/Button.js
+++ src/Component/Button.js
@@ -4,27 +4,28 @@ function randomGetal(min, max) {
     return Math.floor(Math.random() * (max - min)) + min;
 }
 
-function movePosition(x, y) {
-    randomGetal()
-}
-
-//console.log(movePosition(-100, 100))
-
 class Button extends React.Component {
     constructor(props) {
         super(props);
-        this.state = { x: movePosition(), y: movePosition() }
+        this.state = { x: randomGetal(0, 100), y: randomGetal(0, 100) }
     }
     render() {
+        console.log(this.state)
         return (
-            <button style={{ "marginTop": "100px", "marginLeft": "100px" }}
-                onClick={() => {
-                    movePosition();
-                    movePosition()
-                }
-                }>
-                click me!
+            <div>
+                ({this.state.x},{this.state.y})
+                <button style={{ "marginTop": this.state.x + "px", "marginLeft": this.state.y + "px" }}
+                    onClick={() => {
+                        this.setState({
+                            x: randomGetal(0, 100),
+                            y: randomGetal(0, 100)
+
+                        })
+                    }
+                    }>
+                    click me!
             </button >
+            </div>
         )
     }
 }
